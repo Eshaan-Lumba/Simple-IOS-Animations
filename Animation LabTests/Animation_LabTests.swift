c29fa4ad2f4929689936ccceff410d8eb3cd419f
Eshaan Lumba <eshaan100102@gmail.com>
2021-03-08 18:51:30 +0400

    Initial Commit

---
+++ Animation LabTests/Animation_LabTests.swift
@@ -0,0 +1,33 @@
+//
+//  Animation_LabTests.swift
+//  Animation LabTests
+//
+//  Created by Eshaan Lumba on 08/03/2021.
+//
+
+import XCTest
+@testable import Animation_Lab
+
+class Animation_LabTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
